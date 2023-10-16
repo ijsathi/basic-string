194892c75667f2a2e0dcd81643cfaa9882a2f229
unknown <ijsathi2001rs@gmail.com>
2023-10-16 14:51:20 +0600

    fgets

--- input.c
+++ input.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    char a[9];
-    gets(a);
-    scanf("%s", &a);
+    char a[18];
+    fgets(a, 18, stdin);
+    a[6] = '\0'; // fgets er jekhane null korate cai setar index e evabe null dey
     printf("%s", a);
     return 0;
 }
