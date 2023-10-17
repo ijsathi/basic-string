de048ec3b8aef4b5a03b98de3c1511ea6e056cfe
unknown <ijsathi2001rs@gmail.com>
2023-10-17 13:51:37 +0600

    e. count

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,3 +1,15 @@
-for(int i=0; i< 5; i++){
-    //     scanf("%c", &a[i]);
-    // }
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char a[1000001];
+    scanf("%s", a);
+    int sum = 0;
+    for (int i = 0; i < strlen(a); i++)
+    {
+        sum = (a[i] - '0') + sum;
+    }
+    printf("%d\n", sum);
+    return 0;
+}
